894b85f48a532e223340e9cf931b5aaafc834b56
Nate Ebel <n8ebel@goobar.dev>
2022-07-25 12:42:05 -0600

    Initial commit for slack notification service

---
+++ remote-config-slack-notification-service/app/src/main/kotlin/goobar/dev/remoteconfig/slacknotification/RemoteConfigDiff.kt
@@ -0,0 +1,10 @@
+package goobar.dev.remoteconfig.slacknotification
+
+/**
+ * Holds a diff between two Parameters or Conditions
+ */
+data class RemoteConfigDiff<T>(
+    val key: String,
+    val original: T,
+    val updated: T
+)
